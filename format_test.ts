35d5001f99ad207cebcff34d426526ae4cf7425c
rista404 <risticnikola@outlook.com>
2019-07-15 11:39:44 +0200

    Update links

--- format_test.ts
+++ format_test.ts
@@ -1,6 +1,10 @@
 // Copied from https://github.com/defunctzombie/node-util/blob/master/test/node/format.js
-import { test } from "https://deno.land/x/testing/mod.ts";
-import { assert, assertEquals, assertStrictEq } from "https://deno.land/std/testing/asserts.ts";
+import { test } from "https://deno.land/std@v0.3.0/testing/mod.ts";
+import {
+  assert,
+  assertEquals,
+  assertStrictEq
+} from "https://deno.land/std/testing/asserts.ts";
 import format from "./format.ts";
 
 test(function testFormat() {
@@ -39,7 +43,7 @@ test(function testFormat() {
   assertEquals(format("%%%s%%", "hi"), "%hi%");
   assertEquals(format("%%%s%%%%", "hi"), "%hi%%");
 
-  (function () {
+  (function() {
     let o: { o?: any } = {};
     o.o = o;
     assertEquals(format("%j", o), "[Circular]");
